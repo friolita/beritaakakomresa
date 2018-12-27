4950657b82fd04f6ac30633f42f28d1354697f5b
friolita <friolita@gmail.com>
2018-12-27 16:09:52 +0700

    test commit

---
+++ app/src/main/java/com/gmail/friolita3/beritaakakomresa/DataBerita.java
@@ -0,0 +1,8 @@
+package com.gmail.friolita3.beritaakakomresa;
+
+public class DataBerita {
+    public String judulBerita;
+    public String isiBerita;
+    public String tglPosting;
+    public String gambarBerita;
+}
